46315b413f0d6a3c329f29d6d47428316b4748fc
kuno2582 <kuno9411@gmail.com>
2020-10-29 17:40:21 +0900

    clear ex2884

--- baekjoon/ex2884.py
+++ baekjoon/ex2884.py
@@ -1,1 +1,20 @@
-print('hi')
+a, b = map(int, input().split())
+
+
+def cal_time(h, m, diff_m):
+    result_m = m + diff_m
+    while result_m < 0:
+        h = h - 1
+        result_m = 60 + result_m
+    while result_m > 59:
+        h = h + 1
+        result_m = result_m - 60
+    while h < 0:
+        h = 24 + h
+    while h > 23:
+        h = h - 24
+
+    print(h, result_m)
+
+
+cal_time(a, b, -45)
